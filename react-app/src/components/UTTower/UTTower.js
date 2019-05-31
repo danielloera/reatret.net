3655a681434c525027e5e116bdbe81b8c5512d44
Daniel Matthew Loera <danny.reatret@gmail.com>
2019-05-30 19:32:06 -0500

    convert UTTower to use hooks

--- react-app/src/components/UTTower/UTTower.js
+++ react-app/src/components/UTTower/UTTower.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
 import Paper from '@material-ui/core/Paper'
@@ -20,64 +20,54 @@ const styles = (theme) => ({
   },
 })
 
-class UTTower extends React.Component {
+function UTTower(props) {
+  const [ width, setWidth ] = useState(0)
+  const [ height, setHeight ] = useState(0)
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      width: 0,
-      height: 0
-    }
-  }
-
-  updateDimensions() {
+  function updateDimensions() {
     const ratio = window.innerWidth / window.innerHeight
     const opRatio = window.innerHeight / window.innerWidth
     const defaultWidth = window.innerWidth * 0.80
     const defaultHeight = window.innerHeight * 0.80
     const height = ratio > 1.5 ? window.innerHeight / ratio : window.innerHeight / opRatio
     const width = ratio > 1.5 ? window.innerWidth / ratio : window.innerWidth / opRatio
-    this.setState({
-      width: Math.min(width, defaultWidth),
-      height: Math.max(height, defaultHeight)
-    })
+    setWidth(Math.min(width, defaultWidth))
+    setHeight(Math.max(height, defaultHeight))
   }
 
-  componentDidMount() {
-    this.updateDimensions()
-    window.addEventListener("resize", this.updateDimensions.bind(this))
-  }
+  useEffect(() => {
+    updateDimensions()
+    window.addEventListener("resize", updateDimensions)
+    return () => window.removeEventListener("resize", updateDimensions)
+  }, [])
 
-  render() {
-    const { classes } = this.props
-    const { width, height } = this.state
-    return (
-      <div className={classes.root}>
-        <Paper className={classes.botHolder} elevation={8}>
-          <iframe
-            title="UT Tower"
-            allow="microphone;"
-            width={width}
-            height={height}
-            src="https://console.dialogflow.com/api-client/demo/embedded/uttower">
-          </iframe>
-        </Paper>
+  const { classes } = props
+  return (
+    <div className={classes.root}>
+      <Paper className={classes.botHolder} elevation={8}>
+        <iframe
+          title="UT Tower"
+          allow="microphone;"
+          width={width}
+          height={height}
+          src="https://console.dialogflow.com/api-client/demo/embedded/uttower">
+        </iframe>
+      </Paper>
 
-        <div className={classes.endingText}>
-          <Typography component="p" >
-            This bot was created on {" "}
-            <a href="https://dialogflow.com">Dialogflow</a>
-            {" "}to get lighting updates from the{" "}
-            <a href="https://en.wikipedia.org/wiki/Main_Building_(University_of_Texas_at_Austin)">Tower in UT Austin.</a>
-            <br/>UT Tower bot is available on{" "}
-            <a href="https://assistant.google.com/services/a/uid/0000001deeb5797c?hl=en">Google Assistant</a>
-            {" "} and{" "}
-            <a href="https://www.facebook.com/utexastower">Facebook Messenger.</a>
-          </Typography>
-        </div>
+      <div className={classes.endingText}>
+        <Typography component="p" >
+          This bot was created on {" "}
+          <a href="https://dialogflow.com">Dialogflow</a>
+          {" "}to get lighting updates from the{" "}
+          <a href="https://en.wikipedia.org/wiki/Main_Building_(University_of_Texas_at_Austin)">Tower in UT Austin.</a>
+          <br/>UT Tower bot is available on{" "}
+          <a href="https://assistant.google.com/services/a/uid/0000001deeb5797c?hl=en">Google Assistant</a>
+          {" "} and{" "}
+          <a href="https://www.facebook.com/utexastower">Facebook Messenger.</a>
+        </Typography>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 UTTower.propTypes = {
