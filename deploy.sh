d0700b76430287c242f61a18ae7b439100fbbff4
danielloer <danny.reatret@gmail.com>
2024-05-03 17:42:16 +0000

    update script for next.js static deployment

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-sudo npm run build
-rsync -uvrP --delete-after build/ danny@192.168.50.158:/usr/share/reatret.net/www
+npm run build
+rsync -uvrP --delete-after out/ root@192.168.50.53:/usr/share/reatret.net/www
