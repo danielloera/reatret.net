ec15a07361c954f9fca882939f5d81a645a26d7e
Daniel Loera <danny.reatret@gmail.com>
2024-05-17 11:22:22 -0500

    Add exif data to photo page

--- src/app/photo/[id]/photo.js
+++ src/app/photo/[id]/photo.js
@@ -4,6 +4,8 @@ import { useAppWriteContext } from '../../appwrite_provider';
 import { useState, useEffect } from 'react';
 import './styles.css'
 
+const DEFAULT_EXIF = "N/A";
+
 export default function Photo(props) {
   const [photo, setPhoto] = useState(null);
   const client = useAppWriteContext();
@@ -16,6 +18,28 @@ export default function Photo(props) {
   }, []);
 
   if (photo == null) return (<div className="loader"></div>);
+
+  const shutterSpeed =
+          photo.shutter_speed  == null ?
+          DEFAULT_EXIF :
+          `1/${Math.round(photo.shutter_speed)}`;
+  const focalLength =
+          photo.focal_length == null ?
+          DEFAULT_EXIF :
+          `${photo.focal_length}mm`;
+  const exposureTime =
+          photo.exposure_time  == null ?
+          DEFAULT_EXIF :
+          `1/${Math.round(1/photo.exposure_time)}`;
+  const fNumber =
+          photo.f_number == null ?
+          DEFAULT_EXIF :
+          `f/${photo.f_number}`;
+  const iso = photo.iso ?? DEFAULT_EXIF;
+  const cameraMake = photo.camera_make ?? DEFAULT_EXIF;
+  const cameraModel = photo.camera_model ?? DEFAULT_EXIF;
+  const lensMake = photo.lens_make ?? DEFAULT_EXIF;
+  const lensModel = photo.lens_model ?? DEFAULT_EXIF;
 
   return (
     <main>
@@ -44,6 +68,47 @@ export default function Photo(props) {
           text-base sm:text-xl md:text-2xl">
         {photo.description}
       </p>
+      <table className="
+                rounded-md
+                ms-4 sm:ms-8 lg:ms-12
+                text-base sm:text-lg md:text-xl">
+        <tr>
+          <th>Shutter Speed</th>
+          <td>{shutterSpeed}</td>
+        </tr>
+        <tr>
+          <th>Focal Length</th>
+          <td>{focalLength}</td>
+        </tr>
+        <tr>
+          <th>Exposure Time</th>
+          <td>{exposureTime}</td>
+        </tr>
+        <tr>
+          <th>F Number</th>
+          <td>{fNumber}</td>
+        </tr>
+        <tr>
+          <th>ISO</th>
+          <td>{iso}</td>
+        </tr>
+        <tr>
+          <th>Camera Make</th>
+          <td>{cameraMake}</td>
+        </tr>
+        <tr>
+          <th>Camera Model</th>
+          <td>{cameraModel}</td>
+        </tr>
+        <tr>
+          <th>Lens Make</th>
+          <td>{lensMake}</td>
+        </tr>
+        <tr>
+          <th>Lens Model</th>
+          <td>{lensModel}</td>
+        </tr>
+      </table>
     </main>
   );
 }
