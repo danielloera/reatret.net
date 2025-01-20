d481e111b2a8b87580fa0eede068236122e4e8ab
Daniel Loera <danny.reatret@gmail.com>
2025-01-20 13:23:45 -0600

    Replace a tag with Link in app bar

--- src/app/common/app_bar.js
+++ src/app/common/app_bar.js
@@ -1,4 +1,5 @@
 import "../globals.css";
+import Link from 'next/link';
 
 export default function AppBar(props) {
   return (
@@ -7,7 +8,7 @@ export default function AppBar(props) {
       text-fuchsia-300
       text-xl     ps-4    pt-4    pb-4
       md:text-2xl lg:text-3xl sm:ps-6 sm:pt-4 sm:pb-4">
-        <a href="/">reatret.net</a> {props.buttons}
+        <Link href="/">reatret.net</Link> {props.buttons}
     </div>
   );
 }
