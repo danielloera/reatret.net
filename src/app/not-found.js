d383ed4972c92bab019ab74c4d921b530a40ca1d
Daniel Loera <danny.reatret@gmail.com>
2025-08-24 12:12:52 -0500

    spacing

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,11 +1,10 @@
-    // app/not-found.js
-    import Link from 'next/link';
+import Link from 'next/link';
 
-    export default function NotFound() {
-      return (
-        <div className="pt-mono-regular text-center pt-[33vh] pb-[45vh]">
-          <h2 className="pb-5">Not Found :(</h2>
-          <Link href="/" className="text-fuchsia-300">Return Home</Link>
-        </div>
-      );
-    }
+export default function NotFound() {
+  return (
+    <div className="pt-mono-regular text-center pt-[33vh] pb-[45vh]">
+      <h2 className="pb-5">Not Found :(</h2>
+      <Link href="/" className="text-fuchsia-300">Return Home</Link>
+    </div>
+  );
+}
